6599d3366f2abd2c2d607aaaf8aca24e2d42e1aa
AtiyaMaysha <85635682+AtiyaMaysha@users.noreply.github.com>
2024-03-05 02:36:06 +0600

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 require('./db/config');
 const User = require("./db/User");
 const Product = require("./db/Product")
+
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-comm';
+
 const app = express();
 
 app.use(express.json());
@@ -13,7 +17,12 @@ app.post("/register", async (req, resp) => {
     let result = await user.save();
     result = result.toObject();
     delete result.password;
-    resp.send(result);
+    Jwt.sign({ result }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+        if (err) {
+            resp.send({ result: "something went wrong" })
+        }
+        resp.send({ result, auth: token })
+    })
 });
 
 app.post("/login", async (req, resp) => {
@@ -21,7 +30,13 @@ app.post("/login", async (req, resp) => {
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
-            resp.send(user)
+            Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
+                if (err) {
+                    resp.send({ result: "something went wrong" })
+                }
+                resp.send({ user, auth: token })
+            })
+    
         }
         else {
             resp.send({ result: 'No User Found' })
@@ -36,5 +51,53 @@ app.post("/add-product", async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
     resp.send(result)
+});
+
+app.get("/products", async (req, resp) => {
+    let products = await Product.find();
+    if (products.length > 0) {
+        console.log(products);
+        resp.send(products);
+    } else {
+        resp.send({ result: "No Product found" });
+    }
+});
+
+app.delete("/product/:id", async (req, resp) => {
+    const result = await Product.deleteOne({ _id: req.params.id });
+    resp.send(result);
+});
+
+app.get("/product/:id", async (req, resp) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        resp.send(result)
+    } else {
+        resp.send({ result: "No Result Found." })
+    }
+});
+
+app.put("/product/:id", async (req, resp) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    resp.send(result)
+});
+
+app.get("/search/:key", async (req, resp) => {
+    let result = await Product.find({
+        "$or": [
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } }
+        ]
+    });
+    resp.send(result)
 })
+
+
+
 app.listen(5000);
